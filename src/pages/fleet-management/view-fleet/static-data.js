9c232885e0c1a52dd167cd08f6baa24e5930cabc
kaveeja1999 <87494020+IT21117428@users.noreply.github.com>
2024-03-23 00:15:23 +0530

    View Fleet Page
    Create View Fleet page table

---
+++ src/pages/fleet-management/view-fleet/static-data.js
@@ -0,0 +1,27 @@
+export const rows = [
+  {
+    vehicle_id: '1',
+    vehicle_number: 'KA-01-1234',
+    vehicle_status: 'Active',
+  },
+  {
+    vehicle_id: '2',
+    vehicle_number: 'KA-01-5678',
+    vehicle_status: 'Inactive',
+  },
+  {
+    vehicle_id: '3',
+    vehicle_number: 'KA-01-9012',
+    vehicle_status: 'Active',
+  },
+  {
+    vehicle_id: '4',
+    vehicle_number: 'KA-01-3456',
+    vehicle_status: 'Inactive',
+  },
+  {
+    vehicle_id: '5',
+    vehicle_number: 'KA-01-7890',
+    vehicle_status: 'Active',
+  },
+]
